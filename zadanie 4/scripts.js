b5b5e758207cb437899ccf53bd8d6767329151bf
Tomi Pawlowski <pszennica@gmail.com>
2016-11-29 19:41:21 +0100

    zadanie 4

--- zadanie 4/scripts.js
+++ zadanie 4/scripts.js
@@ -3,21 +3,14 @@
     $(document).ready(function() {
 
         var form = $(".contact-form"),
-            field = form.find(".form-field"),
             wiadomosc = $("#wiadomosc");
 
-
             form.on("submit", function(e){
-
             	e.preventDefault();
             	if ($('input').val() !== "") {
             		wiadomosc.append($('input').val() + "</br>");
             	};
             });
-
-
-
-
     });
 
 })(jQuery);
